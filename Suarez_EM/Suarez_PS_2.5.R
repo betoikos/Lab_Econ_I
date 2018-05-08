a6e34473b944bdbfa556c930a15d60c1f8a21b2b
Evelyn <36285602+EvelynSuarez@users.noreply.github.com>
2018-05-07 22:37:03 -0500

    Add files via upload

---
+++ Suarez_EM/Suarez_PS_2.5.R
@@ -0,0 +1,12 @@
+# Evelyn Magali Suárez Reyes
+#
+#############################################################################################
+##
+##   Tarea 2. Ejercicio 5
+##
+#############################################################################################
+#
+# X una matriz n*k de números reales fijos. b un vector de dimensión k de números
+# reales fijos. U vector aleatorio de dimensión n, E(U)=0, var(U)= sigma^2_u I. Rb=r.
+
+
